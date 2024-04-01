c2408f0b72a895dd235aada8538df326cf1ab23a
Juha Lindstedt <juha@pakastin.fi>
2024-04-01 18:46:08 +0300

    Fix export

---
+++ index.js
@@ -0,0 +1,7 @@
+const cloudflare = require('./data/cf.json');
+const hetzner = require('./data/hetzner.json');
+const upcloud = require('./data/upcloud.json');
+
+exports.cloudflare = cloudflare;
+exports.hetzner = hetzner;
+exports.upcloud = upcloud;
